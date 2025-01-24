2b08f25c03243aa0c7142c4f4c1d961ab53cf1fb
Killian Colin <killian.colin@proton.me>
2025-01-24 22:22:35 +0100

    trying to apply a force to my rectangle

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,17 @@ use bevy::{asset::Assets, prelude::*, window::PresentMode};
 use bevy_kira_audio::prelude::*;
 use main_menu::main_menu_plugin::MainMenuPlugin;
 
+use avian2d::prelude::*;
+
 mod main_menu;
+
+#[derive(Component)]
+enum Player {
+    one,
+    two,
+    three,
+    four,
+}
 
 #[derive(States, Debug, Clone, PartialEq, Default, Eq, Hash)]
 enum MyAppState {
@@ -21,10 +31,25 @@ fn setup_game(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let width = 1.0 * 128.;
+    let height = 0.2 * 128.;
     commands.spawn((
-        Mesh2d(meshes.add(Rectangle::default())),
-        MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::css::PURPLE))),
-        Transform::default().with_scale(Vec3::splat(128.)),
+        RigidBody::Dynamic,
+        Collider::rectangle(width, height),
+        Mesh2d(meshes.add(Rectangle::new(width, height))),
+        MeshMaterial2d(materials.add(Color::from(bevy::color::palettes::css::ORANGE))),
+        Transform::default(),
+        ColliderDensity(2.0),
+        Player::one,
+    ));
+}
+
+//, mut interaction_query: Query<(&Transform), (With<Player>)>
+fn test_force(mut commands: Commands) {
+    commands.spawn((
+        RigidBody::Dynamic,
+        ExternalForce::new(Vec2::Y * 100.),
+        Transform::from_xyz(1.0, 0., 0.),
     ));
 }
 
@@ -49,6 +74,7 @@ pub fn run() {
     }));
     app.init_state::<MyAppState>();
 
+    app.add_plugins(PhysicsPlugins::default());
     app.add_plugins(MainMenuPlugin);
     app.add_plugins(AudioPlugin);
 
@@ -60,6 +86,8 @@ pub fn run() {
     app.add_systems(Startup, setup);
 
     app.add_systems(OnEnter(MyAppState::InGame), setup_game);
+
+    app.add_systems(FixedUpdate, test_force.run_if(in_state(MyAppState::InGame)));
     // app.add_systems(OnEnter(MyAppState::InGame), start_background_audio);
 
     app.run();
